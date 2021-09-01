cfe4a697125220d0ebfa30d3970ac05ae69bf814
SampleUser0001 <ittimfn+github@gmail.com>
2021-09-01 21:40:24 +0900

    別解を追加

--- app/src/app.py
+++ app/src/app.py
@@ -3,5 +3,9 @@ import sys
 sys.path.append('./util')
 from SampleUtil import SampleUtil
 
+# これでもOK。
+# sys.path.append('./')
+# from util.SampleUtil import SampleUtil
+
 if __name__ == '__main__':
   SampleUtil.hello_world()
